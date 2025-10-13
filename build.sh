d92434814ac081f3c0e386556d6ff1a7d3e1a3af
MADMAN-Modding <mad@kmdtech.net>
2025-10-13 12:58:12 -0400

    added double space police

---
+++ build.sh
@@ -0,0 +1,5 @@
+cmake -S . -B build
+
+cmake --build build --config Release
+
+mv build/LinuxCopyPastaBot bot
--- src/main.cpp
+++ src/main.cpp
@@ -18,15 +18,35 @@ int main()
    bot.on_log(dpp::utility::cout_logger());
 
    bot.on_message_create([&bot](const dpp::message_create_t& event){
+      if (event.msg.author.id == 1267638555254063236) {
+         return;
+      }
+ 
       std::string copy_pasta = "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.\nMany computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called Linux, and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.\nThere really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called Linux distributions are really distributions of GNU/Linux!";
 
       std::string msg = event.msg.content.data();
+
+      std::size_t npos = std::string::npos;
+
+      if (msg.find("  ") != npos) {
+         int double_space = msg.find("  ");
+   
+         std::string reply = "DOUBLE SPACE FOUND!";
+
+         if (double_space > 1 && double_space+4 < msg.length()) {
+            std::string sub_double_space = msg.substr(double_space-2, 6);
+
+            reply = std::string("DOUBLE SPACE AT>>>") + sub_double_space + "<<<";
+         } 
+
+
+         event.reply(reply);
+      }
 
       removeCharacters(msg, "- _()*&$^!@#${}[]||\\.,<>?`+\"'~€ƒ‚„…†‡ˆ‰Š‹ŒŽ‘’“”•–—˜™š›œžŸ¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ⟴:ロ‼️⁉️™️ℹ️↔️↕️");
 
       std::transform(msg.begin(), msg.end(), msg.begin(), ::tolower); 
 
-      std::size_t npos = std::string::npos;
 
       if (msg.find(copy_pasta) != npos || event.msg.content.find("!NO_COPY_PASTA!") != npos || event.msg.content.find("!NCP!") != npos) {
          return;
